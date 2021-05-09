a263037d6c83cb5d33ce07d7de3483959c804751
lphilemon <lphilemon23@gmail.com>
2021-05-09 15:34:29 +0300

    Added Implementations

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 
 const express = require("express");
 const request = require("request");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
@@ -53,6 +56,99 @@ app
 app.get("/chapter13", (req,res) => {
   res.render("chapter13");
 });
+
+app.get("/fibonacci", (req,res) => {
+  res.render("fibonacci");
+});
+
+app.get("/factorial", (req,res) => {
+  res.render("factorial");
+});
+
+app.get("/factorials", (req,res) => {
+  res.render("factorials");
+});
+
+app.post("/factorial", (req,res) => {
+
+               // program to find the factorial of a number
+
+                        // take input from the user
+                      //  const number = parseInt(prompt('Enter a positive integer: '));
+                        let number = req.body.Number;
+                        let orig = req.body.Number;
+                        let fact;
+
+                        // checking if number is negative
+                        if (number < 0) {
+                            console.log('Error! Factorial for negative number does not exist.');
+                        }
+
+                        // if number is 0
+                        else if (number === 0) {
+                            console.log(`The factorial of ${number} is 1.`);
+                        }
+
+                        // if number is positive
+                        else {
+                             fact = 1;
+                            for (i = 1; i <= number; i++) {
+                                fact *= i;
+                            }
+                            console.log(`The factorial of ${number} is ${fact}.`);
+                        }
+                        res.render("factorial", {Numbers: orig, Factorial: fact});
+
+});
+
+app.get("/fibonaccis", (req,res) => {
+  res.render("fibonaccis");
+});
+
+
+app.post("/fibonacci", (req,res) => {
+
+
+ // program to generate fibonacci series up to n terms
+
+               // take input from the user
+              //  const number = parseInt(prompt('Enter the number of terms: '));
+               let number = req.body.Number;
+               let orig = req.body.Number;
+               let fibArray = [];
+               let n1 = 0, n2 = 1, nextTerm;
+                
+               console.log('Fibonacci Series:');
+                
+               for (let i = 1; i <= number; i++) {
+                   console.log(n1);
+                   fibArray.push(n1);
+                   nextTerm = n1 + n2;
+                   n1 = n2;
+                   n2 = nextTerm;
+               }
+
+               res.render("fibonacci", {Numbers: orig, FibArray: fibArray});
+
+});
+
+app.get("/stack", (req,res) => {
+  res.render("stack");
+});
+
+app.get("/stacks", (req,res) => {
+  res.render("stacks");
+});
+
+app.get("/hanoi", (req,res) => {
+  res.render("hanoi");
+});
+
+app.get("/queue", (req,res) => {
+  res.render("queue");
+});
+
+
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
